6c8feb4539fc7cd4843b36f0c2e46b7a591b064d
vladlenmarchenko <markelovwtf7@gmail.com>
2023-09-16 20:32:13 +0300

    (fix) added padding for btn`s in HeroCard

--- src/components/HeroCard.tsx
+++ src/components/HeroCard.tsx
@@ -21,7 +21,7 @@ const HeroCard: FC<HeroCardProps> = ({hero}) => {
 
     return (
         <div className={"w-[33vw] mt-5 rounded-2xl border-2 px-10 py-3 shadow-lg"}>
-            <div className={"flex justify-end gap-5 items-center"}>
+            <div className={"flex justify-end gap-5 items-center p-2"}>
                 <Link className={"text-yellow-400"} to={RouteNames.EDIT_HERO_PAGE + "/" + hero.id}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
